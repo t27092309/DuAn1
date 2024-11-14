7b02cee581addf5e77fab150f9a1d26cf148a46f
ThanhTUngEn <thanhtung19192005@gmail.com>
2024-11-14 10:56:14 +0700

    TUng

---
+++ Source/public/Layout/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./sty.css">
+</head>
+<body>
+    <header></header>
+    
+    <main>
+        <div class="list">
+            <div class="h1">
+                <h1>Chọn phân loại sách </h1>
+            </div>
+            <div class="listbook">
+                <h2>Tất cả nhóm sản phẩm :</h2>
+                <nav>
+                    <ul>
+                        <li>Tiểu thuyết</li> <br>
+                        <li>Light Novel</li> <br>
+                        <li>Truyện ngắn</li> <br>
+                        <li>Truyện trinh thám</li> <br>
+                        <li>Kiếm hiệp</li><br>
+                        <li>Tiểu thuyết</li><br>
+                        <li>Truyện ngắn</li><br>
+                        <li>Light Novel</li><br>
+                        <li>Cuoc song</li><br>
+                    </ul>
+                </nav>
+            </div>
+            <div class="price">
+                <h2>Tuỳ chọn giá :</h2>
+                <nav>
+                    <ul>
+                        <li>0 - 100.000 VND</li> <br>
+                        <li>100.000 - 300.000 VND</li> <br>
+                        <li>300.000 - 500.000 VND</li> <br>
+                        <li>500.000 - 1.000.000 VND</li> <br>
+                        <li>1.000.000 VND trở lên</li><br>
+                    </ul>
+                </nav>
+            </div>
+            <div class="Author">
+                <h2>Nhà xuất bản :</h2>
+                <nav>
+                    <ul>
+                        <li>Kim Đồng</li> <br>
+                        <li>Nhã Nam</li> <br>
+                        <li>Minh Thắng</li> <br>
+                        <li>Đông A</li> <br>
+                        <li>Đinh Tị</li><br>
+                    </ul>
+                </nav>
+            </div>
+            <div class="Language">
+                <h2>Language :</h2>
+                <nav>
+                    <ul>
+                        <li>Tiếng Việt</li> <br>
+                        <li>Tiếng Anh</li> <br>
+                        <li>Tiếng Đức</li> <br>
+                    </ul>
+                </nav>
+            </div>
+            <div class="type">
+                <h2>Loại sách :</h2>
+                <nav>
+                    <ul>
+                        <li>Bìa cứng</li> <br>
+                        <li>Bìa mềm</li> <br>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="products">
+            <div class="dea">
+                <div class="box1">
+                    Sắp xếp theo:
+                </div>
+                <div class="box">
+                    Phù hợp
+                </div>
+                <div class="box">
+                    Bán chạy
+                </div>
+                <div class="box">
+                    Yêu thích
+                </div>
+            </div>
+
+            <div class="SP">
+                <div class="SP1">
+                <div>
+                    <img style="width: 100%;" src="<?= $product->img_product ?>" alt="">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+            <div class="SP">
+                <div class="SP1">
+                </div>
+                <div class="SP1">                                      
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+
+</body>
+</html>
