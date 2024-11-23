979637ac670bfc97e58f004dd2984309e2bf3f9b
t27092309 <truongdoxuan796@gmail.com>
2024-11-23 09:42:06 +0700

    abc

--- Source/public/Layout/header.php
+++ Source/public/Layout/header.php
@@ -30,7 +30,7 @@
             </nav>
             <div class="search-bar">
                 <input type="text" placeholder="Tìm kiếm sản phẩm...">
-                <button> <a href="./Source/public/Layout/search.html">Tìm kiếm</a></button>
+                <button> <a href="admin/?act=product-search">Tìm kiếm</a></button>
             </div>
             <div class="header-actions">
 
